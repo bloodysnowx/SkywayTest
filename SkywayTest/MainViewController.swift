865f82f182f20910edfe3a8327c42512654185df
bloodysnowx <bloodysnowx@gmail.com>
2015-12-31 23:10:33 +0900

    add Skyway SDK

---
+++ SkywayTest/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkywayTest
+//
+//  Created by IwasaAtsushi on 2015/12/31.
+//  Copyright © 2015年 IwasaAtsushi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SkywayTest/SkywayTest-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SkywayTest-Bridging-Header.h
+//  SkywayTest
+//
+//  Created by IwasaAtsushi on 2015/12/31.
+//  Copyright © 2015年 IwasaAtsushi. All rights reserved.
+//
+
+#ifndef SkywayTest_Bridging_Header_h
+#define SkywayTest_Bridging_Header_h
+
+#import <SkyWay/SKWPeer.h>
+
+#endif /* SkywayTest_Bridging_Header_h */
