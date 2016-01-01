936062aff90678e32c6b46b90539f0039f49237a
bloodysnowx <bloodysnowx@gmail.com>
2016-01-01 22:40:52 +0900

    wip

--- SkywayTest/SkywayConnection.swift
+++ SkywayTest/SkywayConnection.swift
@@ -11,15 +11,26 @@ import Foundation
 private let SkywayAPIKey = "cd83668a-22cc-4cd4-9d85-b66c0f0f454f"
 private let SkywayDomain = "com.bloodysnow"
 
+protocol SkywayConnectionCallback {
+    func onOpen(givenId: String)
+    func onConnect()
+    func onCall(mediaConnection: SKWMediaConnection)
+    func onClose()
+    func onDisconnected()
+    func onError(error: SKWPeerError)
+    func onReachability()
+}
+
 class SkywayConnection {
     let peer = SKWPeer(apiKey: SkywayAPIKey, domain: SkywayDomain)
     var msLocal: SKWMediaStream?
     var msRemote: SKWMediaStream?
-    var conn: SKWConnection?
+    var conn: SKWMediaConnection?
     var skywayId: String?
     var connected = false
+    let callback: SkywayConnectionCallback
     
-    init() {
-
+    init(callback: SkywayConnectionCallback) {
+        self.callback = callback
     }
 }
